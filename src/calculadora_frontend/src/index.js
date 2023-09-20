0fa6f49a4d126c85251a20d5151b089bfba1bfcc
root <root@Axel>
2023-09-20 15:28:28 -0600

    Primer commit

--- src/calculadora_frontend/src/index.js
+++ src/calculadora_frontend/src/index.js
@@ -1,19 +1,46 @@
-import { calculadora_backend } from "../../declarations/calculadora_backend";
+// src/calculadora_assets/index.js
+async function realizarOperacion() {
+  const num1 = parseInt(document.getElementById("numero1").value);
+  const num2 = parseInt(document.getElementById("numero2").value);
+  const operacion = document.getElementById("operacion").value;
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+  let resultado;
+  switch (operacion) {
+      case "sumar":
+          resultado = await calculadora.sumar(num1, num2);
+          break;
+      case "restar":
+          resultado = await calculadora.restar(num1, num2);
+          break;
+      case "multiplicar":
+          resultado = await calculadora.multiplicar(num1, num2);
+          break;
+      case "dividir":
+          resultado = await calculadora.dividir(num1, num2);
+          break;
+      case "raizCuadrada":
+            resultado = await calculadora.raizCuadrada(num1);
+            break;
+      case "potencia":
+            resultado = await calculadora.potencia(num1, num2);
+            break;
+      case "logaritmo":
+            resultado = await calculadora.logaritmo(num1);
+            break;
+      case "seno":
+            resultado = await calculadora.seno(num1);
+            break;
+      case "coseno":
+            resultado = await calculadora.coseno(num1);
+            break;
+      case "tangente":
+            resultado = await calculadora.tangente(num1);
+            break;
+    default:
+            alert("Operación no reconocida");
+            return;
+          
+  }
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await calculadora_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+  document.getElementById("resultado").innerText = resultado.toString();
+}
